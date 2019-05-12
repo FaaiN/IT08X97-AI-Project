3588d1fa0560660a045b5a92fb26c44066db39c6
FaaiN <nakhooda.faaiza@gmail.com>
2019-05-12 20:46:44 +0200

    Created food item class for nutrition information

---
+++ dieticianAI/agent/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace agent
+{
+    class FoodItem
+    {
+        public string Name { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Sugar { get; set; }
+        public double Fat { get; set; }
+        public double Calories { get; set; }
+        public double Salt { get; set; }
+    }
+}
