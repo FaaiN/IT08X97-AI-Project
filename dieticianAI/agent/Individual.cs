791786b1f015b998d8b8abddc27276de8390aa09
FaaiN <nakhooda.faaiza@gmail.com>
2019-05-14 15:48:25 +0200

    Individual and population class for search and fitness calculation

---
+++ dieticianAI/agent/Individual.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace agent
+{
+    class Individual
+    {
+        public double Fitness { get; set; }
+        public double[] Genes { get; set; }
+        public int GeneLength { get; set; }
+
+        public Individual()
+        {
+
+        }
+
+        // fitness based on fitness for each gene in individual
+        // Individual == FoodItem
+        // gene == nutrient
+        public double CalcFitness()
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ dieticianAI/agent/Population.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace agent
+{
+    class Population
+    {
+        public int PopulationSize { get; set; }
+        public Individual[] Individuals { get; set; } // makes up population
+        public int IndexFittest { get; set; } // index of fittest individual
+        public double FitnessValue { get; set; } // value of fitness for Fittest Individual
+
+        public void initialise(int Size)
+        {
+            // create array of individuals of length Size
+            Individuals = new Individual[Size];
+            for (int i = 0; i < Individuals.Length; i++)
+            {
+                // initialise each Individual element 
+                Individuals[i] = new Individual();
+            }
+        }
+
+        public void CalcFitness()
+        {
+            // Calculate fitness of each individual
+            for (int i = 0; i < Individuals.Length; i++)
+            {
+                Individuals[i].CalcFitness();
+            }
+            // getFittest
+            getFittest();
+        }
+
+        // Finds the fittest individual
+        private Individual getFittest()
+        {
+            double max = 1;
+            IndexFittest = 0;
+            for (int i = 0; i < Individuals.Length; i++)
+            {
+                if (max <= Individuals[i].Fitness)
+                {
+                    max = Individuals[i].Fitness;
+                    IndexFittest = i;
+                }
+            }
+            // gets actual individual with bext fitness
+            FitnessValue = Individuals[IndexFittest].Fitness;
+            return Individuals[IndexFittest];
+        }
+    }
+}
