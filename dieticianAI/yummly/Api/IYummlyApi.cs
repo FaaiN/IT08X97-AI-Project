17e087e581f443596f60c768cc8d789a708eaedd
FaaiN <nakhooda.faaiza@gmail.com>
2019-05-16 21:10:43 +0200

    Added Yummly Api and service

---
+++ dieticianAI/yummly/Api/IYummlyApi.cs
@@ -0,0 +1,19 @@
+﻿using Refit;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yummly
+{
+    [Headers("X-Yummly-App-ID:c5aceb3f", "X-Yummly-App-Key:403ade941562861bd03bc510d178f8fb")]
+    public interface IYummlyApi
+    {
+        [Get("/recipes?q={search}")]
+        Task<SearchResult> SearchRecipe(string search);
+
+        [Get("/recipe/{id}")]
+        Task<Recipe> GetRecipe(string id);
+    }
+}
---
+++ dieticianAI/yummly/Models/SearchResult.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace yummly
+{
+    public class SearchResult
+    {
+        public SearchResult()
+        {
+            Matches = Enumerable.Empty<Recipe>().ToList();
+        }
+
+        public ICollection<Recipe> Matches { get; set; }
+
+    }
+
+    public class Recipe
+    {
+        public Recipe()
+        {
+            Ingredients = Enumerable.Empty<string>().ToList();
+
+            NutritionEstimates = Enumerable.Empty<NutritionEstimate>().ToList();
+        }
+
+        public string Id { get; set; }
+
+        public string RecipeName { get; set; }
+
+        public ICollection<string> Ingredients { get; set; }
+
+        public ICollection<NutritionEstimate> NutritionEstimates { get; set; }
+    }
+
+    public class NutritionEstimate
+    {
+        public string Attribute { get; set; }
+
+        public string Description { get; set; }
+
+        public double Value { get; set; }
+
+        public NutritionUnit Unit { get; set; }
+
+    }
+
+    public class NutritionUnit
+    {
+        public string Name { get; set; }
+
+        public string Abbreviation { get; set; }
+    }
+}
