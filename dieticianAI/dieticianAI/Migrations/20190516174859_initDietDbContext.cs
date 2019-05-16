73a335ee7eac6cf42519562fbf8d2ae8896e46e5
FaaiN <nakhooda.faaiza@gmail.com>
2019-05-16 21:09:46 +0200

    Added Db

---
+++ dieticianAI/dieticianAI/Migrations/20190516174859_initDietDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dieticianAI.Migrations
+{
+    public partial class initDietDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FoodItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    RawData = table.Column<string>(nullable: true),
+                    FAT_KCAL = table.Column<double>(nullable: false),
+                    ENERC_KCAL = table.Column<double>(nullable: false),
+                    PROCNT = table.Column<double>(nullable: false),
+                    CHOLE = table.Column<double>(nullable: false),
+                    CHOCDF = table.Column<double>(nullable: false),
+                    SUGAR = table.Column<double>(nullable: false),
+                    FIBTG = table.Column<double>(nullable: false),
+                    FAT = table.Column<double>(nullable: false),
+                    FASAT = table.Column<double>(nullable: false),
+                    FATRN = table.Column<double>(nullable: false),
+                    WATER = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodItems");
+        }
+    }
+}
